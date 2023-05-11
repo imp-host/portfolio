705df4547abf388f14d76f851fe61c6968dee3fa
impHoster <130117979+impHoster@users.noreply.github.com>
2023-05-11 03:42:24 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// About section functionality.
+// About section functionality.------------------------------------------------------------------------------------------------------
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -17,17 +17,17 @@ function opentab(tabname) {
   window.scrollTo(0, tabContentTop - 220);
 }
 
-// Portfolio section "See More" button functionality.
+// Portfolio section "See More" button functionality.------------------------------------------------------------------------------------------------------
 function openImage() {
   window.open("/Images/coming soon.webp");
 }
 
-// Portfolio section "wip" button functionality.
+// Portfolio section "wip" button functionality.------------------------------------------------------------------------------------------------------
 function openImage1() {
   window.open("/Images/wip.jpg");
 }
 
-// Hamburger Menu functionality
+// Hamburger Menu functionality------------------------------------------------------------------------------------------------------
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
@@ -67,7 +67,7 @@ form.addEventListener('submit', e => {
     .catch(error => console.error('Error!', error.message))
 }) */
 
-// Alert function and google sheets data saving
+// Alert function and google sheets data saving------------------------------------------------------------------------------------------------------
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbzHn5pnLOmoHoo0pPH4loBIvjeFPZl5RjWYQCZhnvtBxQAajpCAeyWY_p_uaXvOFCxRIA/exec";
 const form = document.getElementById("contact-form");
@@ -92,19 +92,19 @@ form.addEventListener("submit", (e) => {
     });
 });
 
-// Function to show alert box
+// Function to show alert box------------------------------------------------------------------------------------------------------
 function showAlert() {
   const alertBox = document.getElementById("alert");
   alertBox.style.display = "flex";
 }
 
-// Function to close alert box
+// Function to close alert box------------------------------------------------------------------------------------------------------
 function closeAlert() {
   const alertBox = document.getElementById("alert");
   alertBox.style.display = "none";
 }
 
-// closing the alert
+// closing the alert------------------------------------------------------------------------------------------------------
 let alert = document.getElementById("alert");
 let close = document.getElementById("close");
 function displayAlert() {
@@ -120,3 +120,70 @@ close.addEventListener("click", hideAlert);
 // if (!localStorage.getItem("alertShown")) {
 //   displayAlert();
 // }
+
+// Toggle button to switch between light and dark theme along with color changes------------------------------------------------------------------------------------------------------
+const themeToggle = document.getElementById("theme-toggle");
+const lightIcon = document.getElementById("light-icon");
+const darkIcon = document.getElementById("dark-icon");
+
+// Function to set blue color for elements with rgb(0, 255, 255) color in light mode
+const setBlueColors = () => {
+  const elements = document.querySelectorAll("*");
+  elements.forEach((element) => {
+    const color = window.getComputedStyle(element).color;
+    if (color === "rgb(0, 255, 255)") {
+      element.style.color = "blue";
+    }
+    if (color === "rgb(255, 255, 255)") {
+      element.style.color = "black";
+    }
+  });
+};
+
+// Function to set original color for elements with blue color in dark mode
+const setOriginalColors = () => {
+  const elements = document.querySelectorAll("*");
+  elements.forEach((element) => {
+    if (element.style.color === "blue") {
+      element.style.color = "rgb(0, 255, 255)";
+    }
+    if (element.style.color === "black") {
+      element.style.color = "rgb(255, 255, 255)";
+    }
+  });
+};
+
+// Function to toggle theme
+const toggleTheme = () => {
+  const body = document.body;
+  const isLightMode = body.classList.toggle("light-mode");
+  if (isLightMode) {
+    localStorage.setItem("mode", "light");
+    darkIcon.classList.add("hide");
+    lightIcon.classList.remove("hide");
+    setBlueColors();
+  } else {
+    localStorage.setItem("mode", "dark");
+    lightIcon.classList.add("hide");
+    darkIcon.classList.remove("hide");
+    setOriginalColors();
+  }
+};
+
+// Event listener for theme toggle button
+themeToggle.addEventListener("click", toggleTheme);
+
+// Set the initial state of the button based on the user's preference
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("mode") === "dark") {
+    document.body.classList.add("dark-mode");
+    lightIcon.classList.add("hide");
+    darkIcon.classList.remove("hide");
+    setOriginalColors();
+  } else {
+    document.body.classList.remove("dark-mode");
+    lightIcon.classList.remove("hide");
+    darkIcon.classList.add("hide");
+    setBlueColors();
+  }
+});
